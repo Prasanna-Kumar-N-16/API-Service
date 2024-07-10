dac55800c07dcd8335c57c90f7e8c63b3108a3f1
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-07-10 00:11:40 -0500

    Added login handler changes

--- handler/login/auth.go
+++ handler/login/auth.go
@@ -1,7 +1,11 @@
 package login
 
 import (
+	"api-service/logger"
 	"api-service/service"
+	"api-service/utils"
+	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,6 +22,32 @@ func NewHandlerLogin(service *service.APIServices) AuthenticationInterface {
 	return &Authenticationhandler{service: *service}
 }
 
-func (h *Authenticationhandler) LoginHandler(ctx *gin.Context) {
+type UserLogin struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
+const (
+	reqBodyParseErr = "request body parsing error"
+)
+
+func (h *Authenticationhandler) LoginHandler(ctx *gin.Context) {
+	var reqBody UserLogin
+	logService := logger.GetLogger()
+	if err := ctx.ShouldBindJSON(&reqBody); err != nil {
+		logService.Errorln("error in API request body parsing reason:", err)
+		utils.APIResponse(ctx, reqBodyParseErr, http.StatusBadRequest, nil)
+		return
+	}
+	reqBody.Password = strings.ReplaceAll(reqBody.Password, " ", "")
+	if reqBody.Email == "" || reqBody.Password == "" {
+		logService.Errorln("empty request body param")
+		utils.APIResponse(ctx, reqBodyParseErr, http.StatusBadRequest, nil)
+		return
+	}
+	if !strings.Contains(reqBody.Email, "@") && !strings.Contains(reqBody.Email, ".") {
+		logService.Errorln("Email format Invalid")
+		utils.APIResponse(ctx, reqBodyParseErr, http.StatusBadRequest, nil)
+		return
+	}
 }
