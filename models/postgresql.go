66c3249ed2d4cbba54bf1ff4741237ef865097df
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-07-24 21:52:09 -0500

    added postgresql to the model

---
+++ models/postgresql.go
@@ -0,0 +1,41 @@
+package models
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+// Config holds the database configuration settings
+type Config struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+	TimeZone string
+}
+
+// Service holds the database connection
+type Service struct {
+	DB *gorm.DB
+}
+
+// NewService creates a new database service
+func NewService(cfg Config) (*Service, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s TimeZone=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode, cfg.TimeZone,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	return &Service{DB: db}, nil
+}
