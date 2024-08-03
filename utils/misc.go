7539aae790cdd87814f5597a3008b075f7525fc0
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-08-03 13:12:07 -0500

    Added OTP

--- utils/misc.go
+++ utils/misc.go
@@ -1,7 +1,22 @@
 package utils
 
-import "strings"
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"strings"
+)
 
 func IsAdminEmail(email, adminDomain string) bool {
 	return strings.HasSuffix(email, "@"+adminDomain)
+}
+
+// Function to generate a random OTP
+func generateOTP(length int) (string, error) {
+	otpBytes := make([]byte, length)
+	_, err := rand.Read(otpBytes)
+	if err != nil {
+		return "", err
+	}
+	otp := base64.StdEncoding.EncodeToString(otpBytes)
+	return otp[:length], nil
 }
