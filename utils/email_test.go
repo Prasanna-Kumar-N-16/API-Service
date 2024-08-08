8b5caf96f5b736e60cbc81af03a9ddc15cbad43d
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-08-07 21:32:49 -0500

    added assert package

--- utils/email_test.go
+++ utils/email_test.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSendOTPEmail(t *testing.T) {
@@ -13,8 +15,14 @@ func TestSendOTPEmail(t *testing.T) {
 	mockClient := &mockSMTPClient{}
 
 	err := emailConfig.sendOTPEmail(mockClient, "recipient@example.com", "TestPortal", "http://testportal.com", "123456")
-	if err != nil {
-		t.Fatalf("expected no error, but got %v", err)
-	}
+	assert.NoError(t, err)
 
+	assert.True(t, mockClient.authCalled, "expected auth to be called")
+	assert.True(t, mockClient.mailCalled, "expected mail to be called")
+	assert.True(t, mockClient.rcptCalled, "expected rcpt to be called")
+	assert.True(t, mockClient.dataCalled, "expected data to be called")
+	assert.True(t, mockClient.quitCalled, "expected quit to be called")
+
+	expectedMsg := "From: testuser@gmail.com\nTo: recipient@example.com\nSubject: Welcome to TestPortal - Your OTP\n\nWelcome to TestPortal!\n\nYour OTP is: 123456\n\nPlease use this OTP to complete your registration.\n\nYou can access the portal here: http://testportal.com\n\nThank you!"
+	assert.Equal(t, expectedMsg, mockClient.message, "expected message to be %q, but got %q", expectedMsg, mockClient.message)
 }
