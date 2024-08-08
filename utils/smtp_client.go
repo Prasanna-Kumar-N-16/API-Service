59fc6309f09a397e1dfa42e2ae4b49589995c4fe
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-08-07 21:17:31 -0500

    added_smtp_methods

--- utils/smtp_client.go
+++ utils/smtp_client.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"crypto/tls"
 	"net/smtp"
 )
 
@@ -19,4 +20,36 @@ type smtpClient struct {
 type smtpClientWriter interface {
 	Write([]byte) (int, error)
 	Close() error
+}
+
+func (c *smtpClient) Auth(auth smtp.Auth) error {
+	return c.client.Auth(auth)
+}
+
+func (c *smtpClient) Mail(from string) error {
+	return c.client.Mail(from)
+}
+
+func (c *smtpClient) Rcpt(to string) error {
+	return c.client.Rcpt(to)
+}
+
+func (c *smtpClient) Data() (smtpClientWriter, error) {
+	return c.client.Data()
+}
+
+func (c *smtpClient) Quit() error {
+	return c.client.Quit()
+}
+
+func newSMTPClient(host string, tlsConfig *tls.Config) (SMTPClient, error) {
+	conn, err := tls.Dial("tcp", host+":"+smtpPort, tlsConfig)
+	if err != nil {
+		return nil, err
+	}
+	client, err := smtp.NewClient(conn, host)
+	if err != nil {
+		return nil, err
+	}
+	return &smtpClient{client: client}, nil
 }
