2245872ca866af7ff4437682b49b8bbc405ce324
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-08-07 21:31:05 -0500

    added unit test case for email

--- utils/email_test.go
+++ utils/email_test.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"testing"
+)
+
+func TestSendOTPEmail(t *testing.T) {
+	emailConfig := EmailConfig{
+		Username: "testuser@gmail.com",
+		Password: "password123",
+	}
+
+	mockClient := &mockSMTPClient{}
+
+	err := emailConfig.sendOTPEmail(mockClient, "recipient@example.com", "TestPortal", "http://testportal.com", "123456")
+	if err != nil {
+		t.Fatalf("expected no error, but got %v", err)
+	}
+
+}
