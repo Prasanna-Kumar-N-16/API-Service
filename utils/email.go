a77d2f2b1209fa082bbaba06d75dd02bc0a1a0a9
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-08-07 21:28:28 -0500

    refactored mock changes

--- utils/email.go
+++ utils/email.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"crypto/tls"
 	"fmt"
 	"net/smtp"
 )
@@ -17,38 +16,24 @@ type EmailConfig struct {
 }
 
 // Function to send an email with OTP
-func (u EmailConfig) sendOTPEmail(email, portalName, portalLink, otp string) error {
+func (u EmailConfig) sendOTPEmail(client SMTPClient, email, portalName, portalLink, otp string) error {
 
 	// Create the email content
 	subject := fmt.Sprintf("Welcome to %s - Your OTP", portalName)
 	body := fmt.Sprintf("Welcome to %s!\n\nYour OTP is: %s\n\nPlease use this OTP to complete your registration.\n\nYou can access the portal here: %s\n\nThank you!", portalName, otp, portalLink)
 	msg := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\n%s", u.Username, email, subject, body)
 
-	// Connect to the SMTP server
-	auth := smtp.PlainAuth("", u.Username, u.Password, smtpHost)
-	tlsConfig := &tls.Config{
-		InsecureSkipVerify: true,
-		ServerName:         smtpHost,
-	}
-	conn, err := tls.Dial("tcp", smtpHost+":"+smtpPort, tlsConfig)
-	if err != nil {
-		return err
-	}
-	client, err := smtp.NewClient(conn, smtpHost)
-	if err != nil {
-		return err
-	}
-
 	// Authenticate
-	if err = client.Auth(auth); err != nil {
+	auth := smtp.PlainAuth("", u.Username, u.Password, smtpHost)
+	if err := client.Auth(auth); err != nil {
 		return err
 	}
 
 	// Set the sender and recipient
-	if err = client.Mail(u.Username); err != nil {
+	if err := client.Mail(u.Username); err != nil {
 		return err
 	}
-	if err = client.Rcpt(email); err != nil {
+	if err := client.Rcpt(email); err != nil {
 		return err
 	}
 
@@ -61,8 +46,7 @@ func (u EmailConfig) sendOTPEmail(email, portalName, portalLink, otp string) err
 	if err != nil {
 		return err
 	}
-	err = wc.Close()
-	if err != nil {
+	if err = wc.Close(); err != nil {
 		return err
 	}
 
