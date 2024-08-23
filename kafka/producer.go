921cb38e89bdc5eb87fa35f732d8959f3fab7c57
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-08-23 18:40:02 -0500

    Added kafka producer produce message

--- kafka/producer.go
+++ kafka/producer.go
@@ -11,3 +11,16 @@ func NewProducer(brokers string) (*k.Producer, error) {
 	}
 	return p, nil
 }
+
+func ProduceMessage(p *k.Producer, topic string, message []byte) error {
+	err := p.Produce(&k.Message{
+		TopicPartition: k.TopicPartition{Topic: &topic, Partition: k.PartitionAny},
+		Value:          message,
+	}, nil)
+	if err != nil {
+		return err
+	}
+	// Wait for message deliveries
+	p.Flush(15 * 1000)
+	return nil
+}
