79a1a769df6839eb31a7cebde6cef21b90a87a1e
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-10-03 20:55:03 -0500

    Added changes to kafka service

--- kafka_service/consumer.go
+++ kafka_service/consumer.go
@@ -44,3 +44,25 @@ func (k *KService) ConsumeMessages(topic string) ([]byte, error) {
 	return msg.Value, nil
 
 }
+
+func (k *KService) NewProducer(brokers string) error {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": brokers})
+	if err != nil {
+		return nil
+	}
+	k.P = p
+	return nil
+}
+
+func (k *KService) ProduceMessage(topic string, message []byte) error {
+	err := k.P.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: 0},
+		Value:          message,
+	}, nil)
+	if err != nil {
+		return err
+	}
+	// Wait for message deliveries
+	k.P.Flush(15 * 1000)
+	return nil
+}
--- service/service.go
+++ service/service.go
@@ -1,10 +1,14 @@
 package service
 
-import "api-service/models"
+import (
+	k "api-service/kafka_service"
+	"api-service/models"
+)
 
 type APIServices struct {
 	MongoDBServices MongoDBServices
 	PostgesQL       *models.Service
+	KafkaService    *k.KService
 }
 type MongoDBServices struct {
 	MongoDBClient *models.DBConnector
