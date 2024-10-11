2b72948557880c0327643a09003138461e0d9b5e
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-10-10 20:19:43 -0500

    Newreportshandler inserted in NewAPIInterface

--- handler/handler.go
+++ handler/handler.go
@@ -2,16 +2,19 @@ package handler
 
 import (
 	"api-service/config"
+	"api-service/handler/dashboard"
 	"api-service/handler/login"
 	"api-service/service"
 )
 
 type APIInterface struct {
-	Auth login.AuthenticationInterface
+	Auth   login.AuthenticationInterface
+	Report dashboard.ReportsInterface
 }
 
 func NewAPIInterface(s *service.APIServices, c config.ConfigStruct) APIInterface {
 	return APIInterface{
-		Auth: login.NewHandlerLogin(s, c),
+		Auth:   login.NewHandlerLogin(s, c),
+		Report: dashboard.NewReportsInt(s, c),
 	}
 }
