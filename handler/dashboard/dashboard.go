b7f544f404fe3d7c12b11406f91e02d2fa831fe7
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-10-10 20:18:29 -0500

    Added changes to Newreportshandler

--- handler/dashboard/dashboard.go
+++ handler/dashboard/dashboard.go
@@ -12,3 +12,7 @@ type Reportshandler struct {
 	service service.APIServices
 	c       config.ConfigStruct
 }
+
+func NewReportsInt(service *service.APIServices, c config.ConfigStruct) ReportsInterface {
+	return &Reportshandler{service: *service, c: c}
+}
