84b33ebec529ca126e51957cf9cb2f5d5a9d7908
Prasanna-Kumar-Nagaboyina-16 <radhaprasanna77@gmail.com>
2024-10-12 22:24:07 -0500

    Added dashboard handler

--- handler/dashboard/dashboard.go
+++ handler/dashboard/dashboard.go
@@ -3,9 +3,12 @@ package dashboard
 import (
 	"api-service/config"
 	"api-service/service"
+
+	"github.com/gin-gonic/gin"
 )
 
 type ReportsInterface interface {
+	DashboardHandler(*gin.Context)
 }
 
 type Reportshandler struct {
@@ -15,4 +18,8 @@ type Reportshandler struct {
 
 func NewReportsInt(service *service.APIServices, c config.ConfigStruct) ReportsInterface {
 	return &Reportshandler{service: *service, c: c}
+}
+
+func (h *Reportshandler) DashboardHandler(c *gin.Context) {
+
 }
